838d206211fa428b5918d7ab09e9b25fd66cf702
hyesung.park <48369800+hpark7@users.noreply.github.com>
2023-04-22 11:10:09 -0400

    PetAction interface is added

---
+++ src/PetActions.java
@@ -0,0 +1,14 @@
+/**
+* interface PetActions.java
+* this interface set feed(), thirst(), and happiness() methods. 
+*
+*/
+public interface PetActions{
+  
+  void feed();
+ 
+  void thirst();
+ 
+  void happiness(); 
+  
+}
